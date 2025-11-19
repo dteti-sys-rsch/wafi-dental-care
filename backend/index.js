1a8c96315ad8c4aff6c1186373d5adfda5204fcb
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2025-11-19 20:41:03 +0700

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const app = express()
+
+// DOTENV
+const dotenv = require('dotenv')
+dotenv.config({ path: './.env' })
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+// CORS
+app.use(cors())
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Wafi Dental Care Backend Service!')
+})
+
+// ROUTE ERROR HANDLING
+app.use((req, res, next) => {
+  const error = new Error('Not found!')
+  error.status = 404
+  next(error)
+})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
+// SERVER
+app.listen(process.env.PORT, () => {
+  console.log('Server is running!')
+})
+
+module.exports = app
